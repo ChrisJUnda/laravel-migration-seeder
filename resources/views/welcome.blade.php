9e193dbe5c5c27bb9db75d20bc394a913aefcaa7
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-10 09:52:52 +0200

    [UPD] NAVBAR ADDED

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
-    @vite('resources/js/app.js')
-</head>
-
-<body>
+@extends('layouts.app')
+@section('main')
     <h1>
-        Prova
+        Homepage
     </h1>
-
-    <img src="{{ Vite::asset('resources/img/duck.jpg') }}" alt="Paperella Laravel Template">
-
-</body>
-
-</html>
+    <p>
+        Sono homepage
+    </p>
+@endsection
