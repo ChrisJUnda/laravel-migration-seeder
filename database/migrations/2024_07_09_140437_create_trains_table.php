afef3c2b7370b75fe57c5b199c05da89d826e96f
ChrisJUnda <christian.segoviaunda@gmail.com>
2024-07-10 16:21:09 +0200

    [FIX] FIX LIST

--- database/migrations/2024_07_09_140437_create_trains_table.php
+++ database/migrations/2024_07_09_140437_create_trains_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('company', 50);
-            $table->string('departue_station', 100);
+            $table->string('departure_station', 100);
             $table->string('arrival_station', 100);
             $table->dateTime('departure_time');
             $table->dateTime('arrival_time');
